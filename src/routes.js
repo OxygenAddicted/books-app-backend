2fe596e73a215a87a20b7d2d4bee5a8ebae0f19a
OxygenAddicted <alimamul.luthfi@gmail.com>
2023-10-08 19:23:45 +0700

    first commit

---
+++ src/routes.js
@@ -0,0 +1,42 @@
+const {
+  addBookHandler,
+  getAllBooksHandler,
+  getBookByIdHandler,
+  updateBookByIdHandler,
+  deleteBookByIdHandler,
+} = require('./handlers')
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBookByIdHandler,
+  },
+
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: updateBookByIdHandler,
+  },
+
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: deleteBookByIdHandler,
+  },
+
+]
+
+module.exports = routes
